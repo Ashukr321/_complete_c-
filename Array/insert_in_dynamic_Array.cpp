68aa9f00229d5bd1cc1e1e3d23aabf9a047b4f53
Ashukr321 <ashutoshkumarsingh55555@gmail.com>
2023-06-29 18:58:28 +0530

    vector and iterator is completed

---
+++ Array/insert_in_dynamic_Array.cpp
@@ -0,0 +1,45 @@
+// dynamic array  => vector
+
+#include<iostream>
+
+// include the  iterator 
+#include<iterator>
+
+// import the vector 
+#include <vector>
+
+using namespace std; 
+void printArray(vector<int> v);
+int main(){
+
+    // create  vector 
+    vector <int> v;
+    //create the ptr iterator 
+    vector<int>::iterator ptr;
+
+    cout<<v.size()<<endl;
+    v.push_back(34);
+    v.push_back(34);
+    v.push_back(34);
+    v.push_back(34);
+    v.push_back(34);
+    // printArray(v);
+
+    for (ptr  = v.begin();  ptr<v.end();ptr++)
+    {
+       cout<<*ptr<<" ";
+    }
+    
+    
+    
+    
+
+    return 0; 
+}
+void printArray(vector <int>v){
+    for (int i :v)
+    {
+      cout<<i<<" ";
+    }
+    
+}
