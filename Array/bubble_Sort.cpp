8113e74749d4b772e5f527e947d577199fbab4ba
Ashukr321 <ashutoshkumarsingh55555@gmail.com>
2023-07-01 13:27:47 +0530

    bubble sort is completed

---
+++ Array/bubble_Sort.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+
+void bubbleSort(int arr[], int n)
+{
+    for (int i = 0; i < n-1; i++)
+    {
+        for (int j = 0; j <n-1-i; j++)
+        {
+            if (arr[j]>arr[j+1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1]= temp;
+            }
+            
+        }
+        
+    }
+    
+}
+int main()
+{
+    int arr[] = {5, 4, 33, 2, 3, 4, 69};
+    int n = sizeof(arr) / sizeof(int);
+    bubbleSort(arr, n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
