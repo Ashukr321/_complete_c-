f77eb09feb21777a30d8584fd0d569aba7076a81
Ashukr321 <ashutoshkumarsingh55555@gmail.com>
2023-07-08 20:48:45 +0530

    completed insertion sort

--- Array/bubble_sort2.cpp
+++ Array/bubble_sort2.cpp
@@ -19,10 +19,10 @@ void swap(int *a, int *b)
 // buble sort
 void bubbleSort(int arr[], int n)
 {
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n - 1; i++)
     {
         int flag = 0;
-        for (int j = 0; j < n - 1; j++)
+        for (int j = 0; j < n - 1 - i; j++)
         {
             if (arr[j] > arr[j + 1])
 
---
+++ Array/insertion_sort_v2.cpp
@@ -0,0 +1,49 @@
+// insertion_sort.cpp
+#include <iostream>
+using namespace std;
+
+// printArray
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; ++i)
+    {
+        cout << arr[i] << " ";
+    }
+}
+
+// insertion sort
+void insertionSort(int arr[], int n)
+{
+    for (int i = 1; i < n; i++)
+
+    {
+        int key =arr[i];
+        int j = i-1; 
+        while (j>=0&& arr[j]>key)
+        {
+            arr[j+1]=arr[j];
+            --j;
+        }
+        arr[j+1]= key;
+    }
+}
+int main()
+{
+    // create the array
+    int arr[] = {
+        2,
+        4,
+        3,
+        3,
+        2,
+        4,
+        56,
+        64,
+        32,
+    };
+    int n = sizeof(arr) / sizeof(int);
+    insertionSort(arr,n);
+    printArray(arr,n);
+    return 0;
+}
+// time complexity o(n^2);
